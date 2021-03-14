181a6a069b4794ce77fb2cab38d002bea5904b78
artofprogramming933 <rbj.aop@gmail.com>
2021-03-14 19:18:05 +0800

    Quiz

--- application/controllers/QuizController.js
+++ application/controllers/QuizController.js
@@ -1,8 +1,14 @@
 const Controller = require('express').Router()
 
 
-Controller.get('/', function(request, response, next){
-  response.render('quiz')
+Controller.get('/:page', function(request, response, next){
+  // console.log(request.params.page)
+  response.render('quiz-'+request.params.page+'', {
+    title: 'Quiz Pesawat Sederhana',
+      subtitle: `Usaha dan Pesawat 
+      Sederhana dalam 
+      Kehidupan Sehari-hari`
+  })
 })
 
 module.exports = Controller
